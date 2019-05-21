95761b022f181cf2147df2276ea669bcbeae596b
Andrea Jemison <andreadjemison@yahoo.com>
2019-05-21 09:01:14 -0400

    added stuff to database

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+// const bookdb = () => {
+//     return mongoose.connect(process.env.DATABASE_URL);
+//   }
 
 if(process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI)
@@ -16,4 +19,4 @@ mongoose.connection.once('open', () => {
     console.log("Mongoose has connected to MongoDB")
 })
 
-module.exports = mongoose
+module.exports = mongoose 
