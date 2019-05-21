8c4b697faa760ae812d17c4e4e5c0356c96212c8
Andrea Jemison <andreadjemison@yahoo.com>
2019-05-21 09:03:19 -0400

    updated read create on index and single

--- server.js
+++ server.js
@@ -1,37 +1,61 @@
 const express = require('express')
 const app = express()
+
 const logger = require('morgan')
 const bodyParser = require('body-parser')
-const bookApi = require('./api/books')
+const cors = require('cors')
 
-app.use(logger('dev'))
+const bookApi = require('./api/books')
+const booksroutes = express.Router()
+// let bookdb = require('./db/books')
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+app.use('/', booksroutes)
 app.use('/client/public', express.static("public"))
-app.use(express.static(`${__dirname}/client/build`))
+// app.use(express.static(`${__dirname}/client/build`))
 
 
 // list of all books
-app.get('/',(req, res)=>{
-    bookApi.allBooks()
-    .then(books => {
-      res.send(books);
-    })
+// app.get('/',(req, res)=>{
+//     bookApi.allBooks()
+//     .then(books => {
+//       res.send(books)
+//     })
+// })
+
+booksroutes.route('/').get((req, res)=> {
+  bookApi.allBooks()
+  .then(books => {
+    res.json(books)
+  })
 })
+
 // add new book
-app.get('/new',(req, res)=>{
-    bookApi.newBook(req.body)
-    .then(() => {
-      res.redirect(`/`);
-    })
-})
+// booksroutes.route('/new').post((req, res)=>{
+//   let newbook = new Book(req.body)
+//     bookApi.newBook(newbook.save())
+//     .then(() => {
+//       res.redirect(`/`)
+//     })
+// })
 // show single book
-app.get('/:id',(req, res)=>{
-    bookApi.oneBook(req.params.id)
-    .then(onebook => {
-      res.send(onebook);
-    })
+// app.get('/:id',(req, res)=>{
+//     bookApi.oneBook(req.params.id)
+//     .then(onebook => {
+//       res.send(onebook);
+//     })
+// })
+booksroutes.route('/:id').get((req, res)=> {
+  bookApi.oneBook(req.params.id)
+  .then(book => {
+    res.json(book)
+  })
 })
 // update single book
 app.get('/:id',(req, res)=>{
@@ -40,6 +64,7 @@ app.get('/:id',(req, res)=>{
       res.send(update);
     })
 })
+
 // delete single book
 app.get('/:id',(req, res)=>{
     bookApi.deleteBook(req.params.id)
@@ -51,7 +76,15 @@ app.get('/:id',(req, res)=>{
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/client/public/index.html`)
 })
+// const eraseDatabaseOnSync = true
+// bookdb().then(async ()=>{
+//   if (eraseDatabaseOnSync) {
+//     await Promise.all([
+//       bookApi.deleteAll({})
+//       // models.Message.deleteMany({}),
+//     ])
 
+// }})
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`server working on ${PORT}`)
